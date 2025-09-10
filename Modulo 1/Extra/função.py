98b13dfeb87604e67181c44dd823c6558b5b8a20
Araujozin <thiagoaraujobelardinucci@gmail.com>
2025-09-10 17:39:25 -0300

    Add files via upload

---
+++ Modulo 1/Extra/função.py
@@ -0,0 +1,21 @@
+# exemplo de função sem parametro
+def exibir_mensagem(    ):
+    print("Ola, esta é uma função sem parametros.") 
+
+#Exemplo de função com parametros n1 e n2 sao os parametros
+def soma(n1, n2):
+    print( n1 + n2) 
+
+# Crie uma funçao que recebe 3 parametros e some eles os parametros
+def funcao(tt,uu,oo):
+    print(tt+oo+uu)
+
+
+
+
+
+# Crie uma funçao que exiba na tela uma mensgaem legal
+def escreva():
+  print("Ola mundo.")
+
+escreva()
